0f79f839faa4856ac130cf09d064661b051e608e
luzumi <corataca@gmail.com>
2022-10-28 15:04:13 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Laravel 9 - Parkplatzverwaltung')
+@section('content')
+    <div class="text-center">
+        Welcome to the application
+    </div>
+@endsection
